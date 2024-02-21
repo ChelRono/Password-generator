50db651282c2981714c09ed7078a6ad6e9a2eb35
Valarie <Valarierono97@gmail.com>
2024-02-21 12:24:46 +0300

    Adds the save functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let buttonEl = document.getElementById("button-el")
 let passwordEl = document.getElementById("password-el")
 let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let passwordLength = 12;
+let saveEl = document.getElementById("save-el")
 
 
 
@@ -9,9 +10,14 @@ function passwordOne() {
     for (let i = 0; i <= passwordLength; i++) {
         let randomPassword = Math.floor(Math.random() * chars.length);
         passwordEl.textContent += chars.substring(randomPassword, randomPassword +1);
+        
        }
       
 }
 function generate() {
     passwordOne()
+}
+function save() {
+    let passwordStr = passwordEl.textContent + "-" 
+    saveEl.textContent += passwordStr
 }
